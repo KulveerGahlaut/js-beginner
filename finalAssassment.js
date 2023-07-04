773a90e895824ddc1e547c3f326c372c74dc4ccb
KulveerGahlaut <138605579+KulveerGahlaut@users.noreply.github.com>
2023-07-04 23:24:32 +0530

    JSProof.js

--- finalAssassment.js
+++ finalAssassment.js
@@ -1,20 +1,4 @@
-/*
-Assessment Requirements
-1. Create a variable that can hold a number of NFT's. What type of variable might this be?
-2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
-   The metadata values will be passed to the function as parameters. When the NFT is ready, 
-   you will store it in the variable you created in step 1
-3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
-4. For good measure, getTotalSupply() should return the number of NFT's you have created
-*/
-
-// create a variable to hold your NFT's
 const NFTs = []
-
-// this function will take in some values as parameters, create an
-// NFT object using the parameters passed to it for its metadata, 
-// and store it in the variable above.
-
 function mintNFT (_Name, _EyeColor, _ShirtType, _Bling) {
     const NFT = {
     "name": _Name,
@@ -26,13 +10,6 @@ function mintNFT (_Name, _EyeColor, _ShirtType, _Bling) {
     NFTs.push(NFT);
 console.log("Minted: "+ _Name + "\n")
 
-/*console.log("Minted: "+ _EyeColor)
-console.log("Minted: "+ _ShirtType)
-console.log("Minted: "+ _Bling)*/
-
-}
-// create a "loop" that will go through an "array" of NFT's
-// and print their metadata with console.log()
 function listNFTs() {
     for(let i=0;i<NFTs.length;i++){
         //console.log(NFTs[i])
@@ -50,10 +27,10 @@ function getTotalSupply() {
 }
 
 // call your functions below this line
-mintNFT("Shruti","Black","Checked","Silver Watch");
-mintNFT("Manohar","Blue","Hoodie","Gold Ring");
-mintNFT("Narad","Green","T-shirt","Diamong Earrings");
-mintNFT("Sangita","Yellow","Shirt","Ruby Necklace");
+mintNFT("Kulveer","Black","Checked","Silver Watch");
+mintNFT("Umar","Blue","Hoodie","Gold Ring");
+mintNFT("Tushar","Green","T-shirt","Diamong Earrings");
+mintNFT("Abhishek","Yellow","Shirt","Ruby Necklace");
 mintNFT("Shreyash","White","Kurta","Emerald Payal");
 
 listNFTs();
